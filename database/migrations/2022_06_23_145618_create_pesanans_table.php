1a96aefcc886dfa070728562d0fd82f202c13bfe
mierjaX <kiakgokil123@gmail.com>
2022-06-25 22:40:08 +0700

    ini update an pesanan

---
+++ database/migrations/2022_06_23_145618_create_pesanans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePesanansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesanans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->text('alamat');
+            $table->text('kontak');
+            $table->text('jenisLayanan');
+            $table->integer('berat')->nullable();
+            $table->timestamps();
+            $table->string('harga')->nullable();
+            $table->string('status')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesanans');
+    }
+}
